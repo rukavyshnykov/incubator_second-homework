bd6e610762634f4dbd8bb2d3c8a702441888baa4
rukavyshnykov <82444499+rukavyshnykov@users.noreply.github.com>
2023-09-13 18:51:21 +0300

    checkcheckcheck

--- src/s1-main/App.tsx
+++ src/s1-main/App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import s from './App.module.css'
 import HW1 from '../s2-homeworks/hw01/HW1'
 import HW2 from '../s2-homeworks/hw02/HW2'
-import HW3 from '../s2-homeworks/hw02/HW2'
+import HW3 from '../s2-homeworks/hw03/HW3'
 
 function App() {
     return (
