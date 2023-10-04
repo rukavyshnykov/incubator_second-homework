a0cf9b7fb4639bff698f1f625329d4c1938bb78c
rukavyshnykov <82444499+rukavyshnykov@users.noreply.github.com>
2023-10-04 18:59:22 +0300

    hw7 v5

--- src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
+++ src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
@@ -45,7 +45,7 @@ const SuperRadio: React.FC<SuperRadioPropsType> = ({
         ? options.map((o) => (
               <label key={name + '-' + o.id} className={s.label}>
                   <input
-                      id={id + '-input-' + o.value}
+                      id={id + '-input-' + o.id}
                       className={finalRadioClassName}
                       type={'radio'}
                       name={name}
